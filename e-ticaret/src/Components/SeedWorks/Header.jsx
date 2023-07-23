5cd3ab710b12b941e7f3466b13e2a010fb033396
MuratCihanUludag <muratcihanuludag@hotmail.com>
2023-07-23 22:26:13 +0300

    the foundations of header design were laid

--- e-ticaret/src/Components/SeedWorks/Header.jsx
+++ e-ticaret/src/Components/SeedWorks/Header.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Header() {
   return (
     <nav className='navbar'>
-        Header
+        <div className='container'>
+            <div className='nav-left'>
+                <img className='logo' src="./public/img/Logo.png" alt="Logo" />
+                <span>Kitapların Gizli Bahçesi</span>
+            </div>
+                <ul className='nav-right'>
+                    <li>
+                        <NavLink to="/" >AnaSayfa</NavLink>
+                    </li>
+
+                    <li>
+                        <NavLink to="/aboutus">Hakkimizda </NavLink>
+                    </li>
+
+                    <li>
+                        <NavLink to="/communication" >İletişim</NavLink>
+                    </li>
+                    
+                    <li>
+                        <NavLink to="/cart" >Sepetim</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/account">Hesap</NavLink>
+                    </li>
+                </ul>
+        </div>
     </nav>
   )
 }
