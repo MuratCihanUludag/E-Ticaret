82dc0701fe351ebbc9834bd0ae9a23ab6a7bdb72
MuratCihanUludag <muratcihanuludag@hotmail.com>
2023-07-23 22:27:06 +0300

    sidebar started

--- e-ticaret/src/Components/SeedWorks/Container.jsx
+++ e-ticaret/src/Components/SeedWorks/Container.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../../Page/Home";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function Container() {
   return (
     <div>
       <Header />
+      <Sidebar/>
       <section className="section" >
         <Routes>
           <Route path="/" element={<Home />} />
